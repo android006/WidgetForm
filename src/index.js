4d6e37d994d7829057d9bb27480a346a022fcb7d
gaoyueliu <gaoyueliu@vortexinfo.cn>
2020-07-13 14:02:55 +0800

    测试第一版

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import WidgetForm from './lib/components/WidgetForm';
+import WidgetFormItem from './lib/components/WidgetForm/FormItem';
 
 export {
   WidgetForm,
+  WidgetFormItem,
 };
 
 export default {
   WidgetForm,
+  WidgetFormItem,
 };
